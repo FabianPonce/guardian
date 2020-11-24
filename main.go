132e89336d14ceb78064403038ca5072692b1512
Fabian Ponce <fponce@toasttab.com>
2020-11-23 20:26:59 -0500

    stock

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"time"
 )
-
-func Base64Encode(message []byte) []byte {
-	b := make([]byte, base64.StdEncoding.EncodedLen(len(message)))
-	base64.StdEncoding.Encode(b, message)
-	return b
-}
 
 func main() {
 	camera := NewCamera()
@@ -23,7 +16,7 @@ func main() {
 		panic(err)
 	}
 
-	isDog, err := classifier.ContainsDog(Base64Encode(image))
+	isDog, err := classifier.ContainsDog(image)
 	if err != nil {
 		panic(err)
 	}
